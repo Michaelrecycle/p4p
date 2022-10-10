da8346887c51f3168b5756dcbd6eda57bbe2f29d
jli280 <67808129+JL-2644@users.noreply.github.com>
2022-10-10 16:15:44 +1300

    Revert "Add sounds"

--- Assets/Abandoned_Psychiatric_Hospitals/Script/DoorOpenScript.cs
+++ Assets/Abandoned_Psychiatric_Hospitals/Script/DoorOpenScript.cs
@@ -2,12 +2,11 @@
 
 public class DoorOpenScript : MonoBehaviour
 {
-    bool trig, open;
-    public float smooth = 2.0f;
-    public float DoorOpenAngle = 90.0f;
+    bool trig, open;//trig-проверка входа выхода в триггер(игрок должен быть с тегом Player) open-закрыть и открыть дверь
+    public float smooth = 2.0f;//скорость вращения
+    public float DoorOpenAngle = 90.0f;//угол вращения 
     private Vector3 defaulRot;
     private Vector3 openRot;
-     public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +14,14 @@ public class DoorOpenScript : MonoBehaviour
         openRot = new Vector3(defaulRot.x, defaulRot.y + DoorOpenAngle, defaulRot.z);
     }
 
-    public void playAudio()
-    {
-        audioSource.PlayOneShot(Resources.Load<AudioClip>("Sounds/Door"));
-    }
     // Update is called once per frame
     void Update()
     {
-        if (trig)// open
+        if (trig)//открыть
         {
             transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
         }
-        else// close
+        else//закрыть
         {
             transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaulRot, Time.deltaTime * smooth);
         }
@@ -37,8 +32,6 @@ public class DoorOpenScript : MonoBehaviour
 			return;
 		}
         trig = true;
-        playAudio();
-
     }
     //private void OnTriggerExit(Collider coll)//вход и выход в\из  триггера 
     //{
